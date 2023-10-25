c4ffbd8526b94598e7d4fbc8de283230830da27c
kkh <k2ho33333@gmail.com>
2023-10-25 13:04:40 +0900

    2

---
+++ src/main/java/org/example/Member.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Member {
+    private int id;
+    private String userId;
+    private String userPw;
+    private String regDate;
+}
