ed3ed13f547f0a10f5fe391dfedee098e103d1d5
kkh <k2ho33333@gmail.com>
2023-11-06 13:49:44 +0900

    명ë령어 수정

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -22,6 +22,7 @@ public class App {
         ArticleController articleController = new ArticleController();
 
         System.out.println("== Eat Pick 시작 ==");
+        System.out.println("명령어 입력은 숫자로 입력해도 되고 한글로 입력해도 됩니다.");
 
         Container.meneList1();
 
@@ -38,42 +39,59 @@ public class App {
             if (Container.getLoginedMember() == null) {
                 switch (command) {
                     case "로그인":
+                    case "1":
                         memberController.login();
                         break;
                     case "회원가입":
+                    case "2":
                         memberController.join();
                         break;
+                    case "3":
+                        systemController.exit();
+                        return;
                 }
             }
             else if (Container.getLoginedMember() != null) {
                 switch (command) {
-                    case "로그아웃":
-                        memberController.logout();
-                        break;
                     case "게시글 등록":
+                    case "1":
                         articleController.write();
                         break;
                     case "게시글 수정":
+                    case "2":
                         articleController.modify();
                         break;
                     case "게시글 삭제":
+                    case "3":
                         articleController.remove();
                         break;
                     case "나의 게시글":
+                    case "4":
                         articleController.myPost();
                         break;
                     case "전체 게시글":
+                    case "5":
                         articleController.allPost();
                         break;
                     case "카테고리 검색":
+                    case "6":
                         articleController.categoryPost();
                         break;
                     case "음식명 검색":
+                    case "7":
                         articleController.foodNamePost();
                         break;
                     case "브랜드명 검색":
+                    case "8":
                         articleController.brandNamePost();
                         break;
+                    case "로그아웃":
+                    case "9":
+                        memberController.logout();
+                        break;
+                    case "10":
+                        systemController.exit();
+                        return;
                 }
             }
         }
